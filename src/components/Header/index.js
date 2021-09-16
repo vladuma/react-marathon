8ff59132dcf1977cb59a18facaa084951c1b838d
Vlad Duma <vlad.duma@deplabs.com>
2021-09-16 11:47:27 +0300

    update : header block

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,8 @@ const Header = ({ title, descr }) => {
     return (
         <header className={style.root}>
             <div className={style.forest}></div>
+            <div className={style.silhouette}></div>
+            <div className={style.moon}></div>
             <div className={style.container}>
                 { title && (<h1>{title}</h1>) }
                 { descr && (<p>{descr}</p>) }
