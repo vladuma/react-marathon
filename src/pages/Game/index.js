66dba49cbc4fea8004b471cf814cb8443225eae6
Vlad Duma <vlad.duma@deplabs.com>
2021-09-16 12:27:37 +0300

    update : move pokemons to game page

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,17 +1,35 @@
-import { useHistory } from 'react-router-dom'
+import { useState } from 'react';
 import Layout from '../../components/Layout';
-import constants from '../../constants';
+
+import POKEMONS from '../../assets/pokemon.json';
+import PokemonCard from '../../components/PokemonCard';
 
 const GamePage = () => {
-    const history = useHistory();
-    const goHome = () => history.push(constants.HOME_PAGE_SLUG);
+    const [pokemons, setPokemons] = useState(POKEMONS);
+    const selectCardHandler = (id) => {
+        setPokemons(prevState => prevState.map(pokemon => pokemon.id === id ? { ...pokemon, isActive: !(!!pokemon.isActive) } : pokemon));
+    };
+
     return (
         <>
             <Layout
-                title="Game page."
-                colorBg="#000"
+                title="Cards"
+                colorBg="#e2e2e2"
             >
-                <button onClick={goHome}>Go to Home</button>
+                <div className="flex">
+                {
+                    pokemons.map((pokemon) => (<PokemonCard
+                        key={pokemon.id}
+                        id={pokemon.id}
+                        name={pokemon.name}
+                        type={pokemon.type}
+                        values={pokemon.values}
+                        img={pokemon.img}
+                        isActive={pokemon.isActive}
+                        selectCardHandler={selectCardHandler}
+                    />))
+                } 
+                </div>
             </Layout>
         </>
     );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,9 +4,6 @@ import Layout from '../../components/Layout';
 import bg1 from '../../assets/bg1.jpg';
 import bg3 from '../../assets/bg3.jpg';
 
-import POKEMONS from '../../assets/pokemon.json';
-import PokemonCard from '../../components/PokemonCard';
-
 const Home = () => {
   return (
     <>
@@ -25,23 +22,6 @@ const Home = () => {
             <p>
             To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the player's color instead. 
             </p>
-        </Layout>
-        <Layout
-            title="Cards"
-            colorBg="#e2e2e2"
-        >
-            <div className="flex">
-            {
-                POKEMONS.map((pokemon) => (<PokemonCard
-                key={pokemon.id}
-                id={pokemon.id}
-                name={pokemon.name}
-                type={pokemon.type}
-                values={pokemon.values}
-                img={pokemon.img}
-                />))
-            } 
-            </div>
         </Layout>
         <Layout
             title="Layout 3 title"
