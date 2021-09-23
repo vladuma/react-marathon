7fb908e7253815ae792bfde8ef49319fe87008c6
Vlad Duma <vlad.duma@deplabs.com>
2021-09-23 20:51:00 +0300

    update : gameplay

---
+++ src/pages/Game/routes/Board/components/PlayerBoard.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import PokemonCard from '../../../../../components/PokemonCard';
+import style from './style.module.css';
+import cn from 'classnames';
+
+const PlayerBoard = ({ player, cards, onCardClick }) => {
+    const [isSelected, setSelected] = useState(null);
+    return (
+        <>
+            {
+                cards.map((item) => (
+                    <div
+                        className={cn(style.cardBoard, {
+                            [style.selected]: isSelected === item.id,
+                        })}
+                        onClick={() => {
+                            setSelected(item.id);
+                            onCardClick && onCardClick({
+                                ...player,
+                                item,
+                            });
+                        }}
+                    >
+                        <PokemonCard
+                            key={item.id}
+                            id={item.id}
+                            name={item.name}
+                            type={item.type}
+                            values={item.values}
+                            img={item.img}
+                            isActive
+                            minimize
+                            handleChangeSelected={() => {}}
+                        />
+                    </div>
+                ))
+            }
+        </>
+    );
+};
+
+export default PlayerBoard;
--- src/pages/Game/routes/Board/index.js
+++ src/pages/Game/routes/Board/index.js
@@ -1,12 +1,62 @@
-import { useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { PokemonContext } from '../../../../context/pokemonContext';
 import PokemonCard from '../../../../components/PokemonCard';
+import PlayerBoard from './components/PlayerBoard';
 import style from './style.module.css';
+
+const setItemsPossession = (items, possession) => {
+    return items.map(item => ({
+        ...item,
+        possession,
+    }));
+};
 
 const BoardPage = () => {
     const history = useHistory();
+    const [board, setBoard] = useState([]);
+    const [player1, setPlayer1] = useState(setItemsPossession(Object.values(pokemons), 'blue'));
+    const [player2, setPlayer2] = useState([]);
+    const [choosenCard, setChoosenCard] = useState(null);
     const { pokemons } = useContext(PokemonContext);
+    const handleBoardClick = async (position) => {
+        if (choosenCard) {
+            const params = {
+                position,
+                card: choosenCard,
+                board,
+            };
+            const { data: newBoard } = await fetch('', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(params),
+            }).then(res => res.json());
+
+            setBoard(newBoard);
+
+            if (choosenCard.player1) {
+                setPlayer1((prevState) => prevState.filter((item) => item.id !== choosenCard.id));
+            }
+            
+            if (choosenCard.player2) {
+                setPlayer2((prevState) => prevState.filter((item) => item.id !== choosenCard.id));
+            }
+        }
+    };
+
+    useEffect(() => {
+        async function getBoard() {
+            const { data } = await fetch('https://reactmarathon-api.netlify.app/api/board').then(res => res.json());
+            setBoard(data);
+        }
+        async function getPlayer2() {
+            const { data } = await fetch('https://reactmarathon-api.netlify.app/api/create-player').then(res => res.json());
+            setPlayer2(setItemsPossession(data), 'red');
+        }
+        Promise.all([getPlayer2, getBoard]);
+    }, []);
 
     if (Object.keys(pokemons).length === 0) {
         history.replace('/game');
@@ -15,31 +65,33 @@ const BoardPage = () => {
     return (
         <div className={style.root}>
             <div className={style.playerOne}>
-                {
-                    Object.values(pokemons).map(({ id, name, type, img, values}) => (<PokemonCard
-                        key={id}
-                        id={id}
-                        className={style.card}
-                        name={name}
-                        type={type}
-                        values={values}
-                        img={img}
-                        isActive={true}
-                        minimize={true}
-                        handleChangeSelected={() => {}}
-                    />))
-                }
+                <PlayerBoard
+                    player={1}
+                    cards={player1}
+                    onCardClick={(card) => setChoosenCard(card)}
+                />
+            </div>
+            <div className={style.playerTwo}>
+                <PlayerBoard
+                    player={2}
+                    cards={player2}
+                    onCardClick={(card) => setChoosenCard(card)}
+                />
             </div>
             <div className={style.board}>
-                <div className={style.boardPlate}>1</div>
-                <div className={style.boardPlate}>2</div>
-                <div className={style.boardPlate}>3</div>
-                <div className={style.boardPlate}>4</div>
-                <div className={style.boardPlate}>5</div>
-                <div className={style.boardPlate}>6</div>
-                <div className={style.boardPlate}>7</div>
-                <div className={style.boardPlate}>8</div>
-                <div className={style.boardPlate}>9</div>
+                {
+                    board.map((item) => (
+                        <div
+                            key={item.position}
+                            className={style.boardPlate}
+                            onClick={() => !item.card && handleBoardClick(item.position)}
+                        >
+                            {
+                                item.card && <PokemonCard {...item.card} isActive minimize />
+                            }
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
