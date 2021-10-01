88796ee429b16ac4e72a85bc2c91581a1eace4e6
Vlad Duma <vlad.duma@deplabs.com>
2021-10-01 10:34:47 +0300

    feature : close modal on body click

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,14 +1,24 @@
+import { useRef } from 'react';
 import cn from 'classnames';
 import style from './style.module.css';
 
 const Modal = ({ title, children, isOpen, onModalClose }) => {
+    const modalRef = useRef();
     const handleModalClose = () => onModalClose && onModalClose();
+    const handleRootClick = (event) => {
+        if (!modalRef.current.contains(event.target)) {
+            handleModalClose();
+        }
+    };
 
     return (
-        <div className={cn(style.root, {
+        <div
+            className={cn(style.root, {
                 [style.open]: isOpen,
-            })}>
-            <div className={style.modal}>
+            })}
+            onClick={handleRootClick}
+        >
+            <div className={style.modal} ref={modalRef}>
                 <div className={style.head}>
                     {title}
                     <span
