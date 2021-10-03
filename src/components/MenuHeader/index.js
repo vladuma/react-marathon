b2d411a10bc80ee739f778fe975a414437f81ce5
Vlad Duma <vlad.duma@deplabs.com>
2021-10-03 14:32:17 +0300

    feature : get and write startup pokemons

--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -5,6 +5,7 @@ import Menu from '../Menu';
 import NavBar from '../NavBar';
 import Modal from '../Modal';
 import LoginForm from '../LoginForm';
+const API_KEY = 'AIzaSyDulN3LR-G9esYIsIYyLmCRqL5OlbK6tQU';
 
 const MenuHeader = ({ bgActive }) => {
     const history = useHistory();
@@ -21,11 +22,21 @@ const MenuHeader = ({ bgActive }) => {
                 returnSecureToken: true,
             }),
         };
-        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:${isLogin ? 'signInWithPassword' : 'signUp'}?key=AIzaSyDulN3LR-G9esYIsIYyLmCRqL5OlbK6tQU`, options).then(res => res.json());
+        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:${isLogin ? 'signInWithPassword' : 'signUp'}?key=${API_KEY}`, options).then(res => res.json());
 
         if (response.hasOwnProperty('error')) {
             NotificationManager.error(response.error.message, 'Error!');
         } else {
+            if (!isLogin) {
+                const startupPokemons = await fetch('https://reactmarathon-api.herokuapp.com/api/pokemons/starter').then(res => res.json());
+                console.log(startupPokemons);
+                for(const item of startupPokemons.data) {
+                    fetch(`https://pokemon-game-7d203-default-rtdb.europe-west1.firebasedatabase.app/${response.localId}/pokemons.json?auth=${response.idToken}`, {
+                        method: 'POST',
+                        body: JSON.stringify(item),
+                    });
+                }
+            }
             localStorage.setItem('idToken', response.idToken);
             NotificationManager.success('Success!');
             handleLoginClick();
